d9e7df22acb0e612ad74f4a7164c28a8a49e5dec
Mohammad Asad <dustyaxad4@gmail.com>
2020-07-17 02:20:57 +0500

    Files

---
+++ Game/Assets/scripts/Rocket_Behavior.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket_Behavior : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField] float thrustSpeed = 370f;
+    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField] ParticleSystem boost;
+    [SerializeField] ParticleSystem dead;
+    [SerializeField] ParticleSystem success;
+
+    bool gameActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameActive)
+        {
+            thrust();
+            rotation();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Launch":
+                break;
+            case "Finish":
+                gameActive = false;
+                success.Play();
+                Invoke("loadNextLevel",2f);
+                break;
+            default:
+                gameActive = false;
+                dead.Play();
+                Invoke("reloadLevel",2f);
+                break;
+        }
+    }
+
+    private void reloadLevel()
+    {
+        gameActive = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void loadNextLevel()
+    {
+        gameActive = true;
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        int nextLevel;
+        if(currentLevel!=SceneManager.sceneCountInBuildSettings-1)
+        {
+            nextLevel = currentLevel+1;
+        }
+        else
+        {
+            nextLevel = 0;
+        }
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    private void rotation()
+    {
+        rigidBody.angularVelocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed*Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationSpeed*Time.deltaTime);
+        }
+    }
+
+    private void thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * thrustSpeed*Time.deltaTime);
+            boost.Play();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            boost.Stop();
+            audioSource.Stop();
+        }
+    }
+}
---
+++ Game/Assets/scripts/movingObstacle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class movingObstacle : MonoBehaviour
+{
+    [SerializeField] Vector3 movingVector = new Vector3(10f, 0f, 0f);
+    Vector3 startingPos;
+    [SerializeField] float movingRate = 3f;
+    [Range(0, 1)] [SerializeField] float movingFactor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.tag == "level4Obstacle")
+        {
+            if ((gameObject.GetComponent(typeof(SphereCollider)) as Collider).enabled)
+            {
+                Invoke("level4Obstacle", 2f);
+            }
+        }
+        else
+        {
+            level3Obstacle();
+        }
+    }
+
+    private void level4Obstacle()
+    {
+        (gameObject.GetComponent(typeof(SphereCollider)) as Collider).enabled = false;
+    }
+
+    private void level3Obstacle()
+    {
+        float speed = Time.time / movingRate;
+        float value = Mathf.Sin(Mathf.PI * 2f * speed);
+        movingFactor = (value / 2f) + 0.5f;
+        Vector3 abc = movingVector * movingFactor;
+        transform.position = startingPos + abc;
+    }
+}
